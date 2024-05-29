714c95a690b56df3bc9e59246b946fd07efb9710
Domhnall ó Póil <dm.poole@hotmail.com>
2024-05-29 22:36:10 +0100

    [FEAT!] (TACHO) - Added Dynamic Upshift light and upgraded Arduino_GFX to v1.4.7
    BREAKING CHANGE - Good luck!

--- arduino/esp32_tachometer/tachometer/tachometer.ino
+++ arduino/esp32_tachometer/tachometer/tachometer.ino
@@ -6,21 +6,57 @@
 #include "background.h"
 
 //----------Define Display Drivers----------
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-  1 /* CS */, 46 /* SCK */, 0 /* SDA */,
-  2 /* DE */, 42 /* VSYNC */, 3 /* HSYNC */, 45 /* PCLK */,
-  11 /* R0 */, 15 /* R1 */, 12 /* R2 */, 16 /* R3 */, 21 /* R4 */,
-  39 /* G0/P22 */, 7 /* G1/P23 */, 47 /* G2/P24 */, 8 /* G3/P25 */, 48 /* G4/P26 */, 9 /* G5 */,
-  4 /* B0 */, 41 /* B1 */, 5 /* B2 */, 40 /* B3 */, 6 /* B4 */
-);
+#define DISPLAY_DC GFX_NOT_DEFINED
+#define DISPLAY_CS 1
+#define DISPLAY_SCK 46
+#define DISPLAY_MOSI 0
+#define DISPLAY_MISO GFX_NOT_DEFINED
 
-Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
-  bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
-  false /* IPS */, 480 /* width */, 480 /* height */,
-  st7701_type5_init_operations, sizeof(st7701_type5_init_operations),
-  true /* BGR */,
-  10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
-  10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
+#define DISPLAY_DE 2
+#define DISPLAY_VSYNC 42
+#define DISPLAY_HSYNC 3
+#define DISPLAY_PCLK 45
+#define DISPLAY_R0 11
+#define DISPLAY_R1 15
+#define DISPLAY_R2 12
+#define DISPLAY_R3 16
+#define DISPLAY_R4 21
+#define DISPLAY_G0 39
+#define DISPLAY_G1 7
+#define DISPLAY_G2 47
+#define DISPLAY_G3 8
+#define DISPLAY_G4 48
+#define DISPLAY_G5 9
+#define DISPLAY_B0 4
+#define DISPLAY_B1 41
+#define DISPLAY_B2 5
+#define DISPLAY_B3 40
+#define DISPLAY_B4 6
+#define DISPLAY_HSYNC_POL 1
+#define DISPLAY_VSYNC_POL 1
+#define DISPLAY_HSYNC_FRONT_PORCH 10
+#define DISPLAY_VSYNC_FRONT_PORCH 10
+#define DISPLAY_HSYNC_BACK_PORCH 50
+#define DISPLAY_VSYNC_BACK_PORCH 20
+#define DISPLAY_HSYNC_PULSE_WIDTH 8
+#define DISPLAY_VSYNC_PULSE_WIDTH 8
+
+Arduino_DataBus *bus = new Arduino_HWSPI(
+    DISPLAY_DC /* DC */, DISPLAY_CS /* CS */,
+    DISPLAY_SCK /* SCK */, DISPLAY_MOSI /* MOSI */, DISPLAY_MISO /* MISO */);
+
+
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+    DISPLAY_DE, DISPLAY_VSYNC , DISPLAY_HSYNC, DISPLAY_PCLK,
+    DISPLAY_B0, DISPLAY_B1, DISPLAY_B2, DISPLAY_B3, DISPLAY_B4,
+    DISPLAY_G0, DISPLAY_G1, DISPLAY_G2, DISPLAY_G3, DISPLAY_G4, DISPLAY_G5,
+    DISPLAY_R0, DISPLAY_R1, DISPLAY_R2, DISPLAY_R3, DISPLAY_R4,
+    DISPLAY_HSYNC_POL, DISPLAY_HSYNC_FRONT_PORCH, DISPLAY_HSYNC_PULSE_WIDTH, DISPLAY_VSYNC_BACK_PORCH,
+    DISPLAY_VSYNC_POL, DISPLAY_VSYNC_FRONT_PORCH, DISPLAY_VSYNC_PULSE_WIDTH, DISPLAY_VSYNC_BACK_PORCH);
+
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    480 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
+    bus, GFX_NOT_DEFINED /* RST */, st7701_type5_init_operations, sizeof(st7701_type5_init_operations));
 
 //---------Define I/O Pins----------
 #define PWM_CHANNEL 1
@@ -44,11 +80,13 @@ int old_State;
 int move_flag = 0;
 int button_flag = 0;
 int flesh_flag = 1;
+long previousMillis = 0;
+bool shift_light_flash_state = true;
 
 //----------Define Color Variables----------
 #define red 0xE8E4
 #define blue 0x10d8
-#define yellow 0x9381
+#define yellow 0xFFE0
 #define bck TFT_BLACK
 #define wht TFT_WHITE
 
@@ -66,8 +104,15 @@ int flesh_flag = 1;
 #define RPM_MAX 10000
 #define RPM_TRUNC_END 4000
 #define ANGLE_TRUNC_END -120.0f
+#define SHIFT_LIGHT_YELLOW 75.0f
+#define SHIFT_LIGHT_RED 90.0f
+#define SHIFT_LIGHT_FLASH 95.0f
+#define SHIFT_LIGHT_MAX_THICKNESS 30
+#define SHIFT_LIGHT_FLASH_INTERVAL 150
+
+
 int rpm_iteration_step = 0;
-int rpm = 0;
+int rpm = 8500;
 int angle = -180;
 short direction = 1;
 
@@ -147,7 +192,7 @@ void loop() {
 void draw(int rpm) {
   //allows for the non linear nature of some tachometers. i.e. lfa indices go 0,2,4,5,6,7,8,9,10
   angle = compute_angle(rpm);
-  plot_gauge(angle);
+  plot_gauge(angle, rpm);
   gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t *)gauge_background.getPointer(), 480, 480);
 }
 
@@ -159,11 +204,27 @@ int compute_angle(int rpm){
     return map(rpm, RPM_TRUNC_END, RPM_MAX, ANGLE_TRUNC_END, ANGLE_END);
   }
 }
-void plot_gauge(int angle) {
+
+void plot_gauge(int angle, int rpm) {
 
   gauge_background.pushImage(0, 0, 480, 480, background);
   gauge_background.setTextColor(YELLOW, TFT_TRANSPARENT);
-  gauge_background.drawString(String(rpm), CLOCK_R, CLOCK_R * 0.75);
+  float rpm_percentage = ((float) rpm / (float) RPM_MAX) * 100.0f;
+
+  //TODO - refactor this into it's own function (drawing shift light)
+  if(rpm_percentage >= SHIFT_LIGHT_YELLOW) {
+    if(rpm_percentage <= SHIFT_LIGHT_FLASH || shift_light_flash_state ){
+      gauge_background.drawSmoothArc(CLOCK_R, CLOCK_R+1, 143, 0, 0, 360, (rpm_percentage <= SHIFT_LIGHT_RED ? yellow : red), wht, false);
+      gauge_background.drawSmoothArc(CLOCK_R, CLOCK_R+1, (rpm_percentage <= SHIFT_LIGHT_RED ? map(rpm_percentage, SHIFT_LIGHT_YELLOW, SHIFT_LIGHT_RED, 143, 110) : 110), 0, 0, 360, bck, (rpm_percentage <= SHIFT_LIGHT_RED ? yellow : red), false);
+    }
+  }
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= SHIFT_LIGHT_FLASH_INTERVAL){
+    shift_light_flash_state = !shift_light_flash_state;
+    previousMillis = currentMillis;
+  }
+
+  //TODO - refactor this into it's own function (drawing the needle)
   int angle_int = (int)trunc((angle));
   if (angle < 0) {
     gauge_background.drawWideLine(CLOCK_R, CLOCK_R, needle_end_x[360 + angle_int], needle_end_y[360 + angle_int], 6.0f, BLUE);
